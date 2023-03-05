b1a396e28fa666418e27c2c866483bc37d3781cc
Cauê <54822334+Caue-Aron@users.noreply.github.com>
2023-03-05 18:38:27 -0300

    Adjust to the examples and code cleanup

---
+++ assets/3D defold logo/defold.go
@@ -0,0 +1,50 @@
+components {
+  id: "lit_mesh"
+  component: "/assets/3D defold logo/defold.model"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "light_reciever"
+  component: "/definitely-lit/light_reciever.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "ambient"
+    value: "1.0, 1.0, 1.0, 0.35"
+    type: PROPERTY_TYPE_VECTOR4
+  }
+}
+components {
+  id: "defold_logo"
+  component: "/assets/3D defold logo/defold_logo.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
